56e9f31846de32850bd1765ba3d15a03327e8a71
sabsiru <zero013@naver.com>
2023-04-12 14:48:49 +0900

    2743 pass

---
+++ src/ProcessionSum.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class ProcessionSum {
+    public static void main(String[] args) {
+        //2738번
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
---
+++ src/WordLength.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class WordLength {
+    public static void main(String[] args) {
+        //2743번 문제
+        Scanner scanner = new Scanner(System.in);
+        String word=scanner.next();
+
+        int number=word.length();
+        System.out.println(number);
+    }
+
+}
