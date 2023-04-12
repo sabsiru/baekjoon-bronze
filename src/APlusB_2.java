4a514bfa5b082c3285872aa5fec139a004ce544a
sabsiru <zero013@naver.com>
2023-04-12 13:57:02 +0900

    2558 pass

---
+++ src/APlusB_2.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class APlusB_2 {
+    //2558번
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a=scanner.nextInt();
+        int b=scanner.nextInt();
+
+        System.out.println(a + b);
+    }
+}
