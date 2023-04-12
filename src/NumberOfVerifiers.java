72a5f171c12faec0bf286c397a20197f8bfb5938
sabsiru <zero013@naver.com>
2023-04-12 14:09:42 +0900

    2475 simple

--- src/NumberOfVerifiers.java
+++ src/NumberOfVerifiers.java
@@ -4,22 +4,12 @@ public class NumberOfVerifiers {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         //2475번 문제
-        //5개의 숫자를 배열을 선언하고
-        int a[] = new int[5];
-        //입력받은 숫자의 제곱을 담을 배열을 선언하고
-        int sum[]=new int[5];
-        //그 합을 담을 변수를 선언한다.
-        int result=0;
-
-        //입력받은 각 숫자의 제곱을 담을 반복문
-        for (int i = 0; i < 5; i++) {
-            a[i] = scan.nextInt();
-            sum[i]=a[i]*a[i];
+        //더 간결한 소스
+        int sum=0;
+        for (int i=0; i<5;i++){
+            int number=scan.nextInt();
+            sum += number*number;
         }
-        //그 수들을 모두 더 할 반복문
-        for(int i=0;i<sum.length;i++) {
-            result += sum[i];
-        }
-        System.out.println(result%10);
+        System.out.println(sum % 10);
     }
 }
