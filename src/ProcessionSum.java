27830caeff930f6f119fcff2d67b4945f3a54a58
sabsiru <zero013@naver.com>
2023-04-12 14:50:03 +0900

    not yet

--- src/ProcessionSum.java
+++ src/ProcessionSum.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class ProcessionSum {
     public static void main(String[] args) {
-        //2738번
+        //2738번 아직
         Scanner scanner = new Scanner(System.in);
 
     }
