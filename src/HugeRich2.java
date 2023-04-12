70b55bc93017a5074fd44af775ec051f3e2e34dd
sabsiru <zero013@naver.com>
2023-04-12 13:01:14 +0900

    first commit

--- src/HugeRich2.java
+++ src/HugeRich2.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class HugeRich2 {
+import java.util.Scanner;
+import java.math.BigInteger;
+
+public class HugeRich2 {
+    public static void main(String[] args) {
+        //int 는 10^1000의 데이터를 받기에 작아서
+        //biginter를 씀
+        Scanner scan=new Scanner(System.in);
+        BigInteger money=scan.nextBigInteger();
+        BigInteger life=scan.nextBigInteger();
+        scan.close();
+        System.out.println(money.divide(life));
+        System.out.println(money.remainder(life));
+    }
+
 }
