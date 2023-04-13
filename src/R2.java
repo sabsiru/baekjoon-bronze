6fec238265af0998f0bbdbcb1b9314d463f991cf
sabsiru <zero013@naver.com>
2023-04-13 10:41:20 +0900

    3046 pass

---
+++ src/R2.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class R2 {
+    public static void main(String[] args) {
+        //3046번
+        Scanner scan=new Scanner(System.in);
+
+        int r1=scan.nextInt();
+        int s=scan.nextInt();
+        int r2=(s*2)-r1;
+
+        System.out.println(r2);
+    }
+
+}
