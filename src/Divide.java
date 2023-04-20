18de120519111fb706f18373b1d1ab5ffc708bf8
sabsiru <zero013@naver.com>
2023-04-20 15:12:24 +0900

    1075번 맨 끝 2자리를 계산

--- src/Divide.java
+++ src/Divide.java
@@ -12,20 +12,16 @@ public class Divide {
         //N의 가장 뒤 두 자리를 새로운 정수로 만든다.
         int newN = n - (n % 100);
         //새로운 정수의 일의 자리부터 1씩 증가시키면서 F로 나누어 떨어지는 수를 찾는다.
-        for(int i = 0; i < 100; i++){
-            if((newN + i) % f == 0){
+        for (int i = 0; i < 100; i++) {
+            if ((newN + i) % f == 0) {
                 //만약 나누어 떨어진다면, 그 수의 일의 자리를 출력한다.
-                if(i < 10){
+                if (i < 10) {
                     System.out.println("0" + i);
-                }else{
+                } else {
                     System.out.println(i);
                 }
                 break;
             }
         }
-
-
-
-
     }
 }
